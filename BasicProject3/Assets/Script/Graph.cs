454cc26150e6756d405edd0041a402f20a09e105
lhjack <lhjackgod@163.com>
2024-12-27 20:16:11 +0800

    finish basicalproject3

--- BasicProject3/Assets/Script/Graph.cs
+++ BasicProject3/Assets/Script/Graph.cs
@@ -2,6 +2,7 @@ using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Graph : MonoBehaviour
 {
@@ -10,27 +11,36 @@ public class Graph : MonoBehaviour
     [SerializeField, Range(10, 100)]
     int resolution = 10;
     Transform[] points;
+    [SerializeField]
+    FunctionLibrary.FUNCTIONNAMES function;
+    private float step;
     private void Awake()
     {
-        points = new Transform[resolution];
-        float step = 2.0f / (float)resolution;
-        for(int i = 0; i < resolution; i++)
+        points = new Transform[resolution * resolution];
+
+        step = 2.0f / (float)resolution;
+        for (int i = 0; i < resolution * resolution; i++)
         {
             points[i] = Instantiate(paraentPosition);
-            Vector3 position = Vector3.zero;
-            position.x = (i + 0.5f) * step - 1.0f;
-            points[i].localPosition = position;
+            points[i].parent = this.transform;
             points[i].localScale = Vector3.one * step;
         }
     }
     private void Update()
     {
+        FunctionLibrary.Funcation f = FunctionLibrary.get_function(function);
         float time = Time.time;
-        for(int i = 0; i < resolution; i++)
+        float u, v = 0.5f * step - 1.0f;
+        for(int i = 0, x = 0, z = 0; i < resolution * resolution; i++, x++)
         {
-            Vector3 position = points[i].localPosition;
-            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
-            points[i].localPosition = position;
+            if(x == resolution)
+            {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1.0f;
+            }
+            u = (x + 0.5f) * step - 1.0f;
+            points[i].localPosition = f(u, v, time);
         }
     }
 }
